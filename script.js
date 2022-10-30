81ea007cf87f87789c1759000e17c417e6db70c8
Brane12345 <95983411+Brane12345@users.noreply.github.com>
2022-10-30 12:39:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jackson", "Jason", "Paul", "Frank", "Larry", "Paul", "Lane", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
     if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
